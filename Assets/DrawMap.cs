5d206ed3d598d955e07989683b6e211cdc8ef9a1
Kohtla <MKmoolooo@yandex.ru>
2017-07-07 13:33:53 +0400

    Генерится карта
    но вылетают ошибки о нарушении индекса массива.

--- Assets/DrawMap.cs
+++ Assets/DrawMap.cs
@@ -3,18 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DrawMap : MonoBehaviour {
-    public GameObject mapPart;
+    public GameObject freePart;
+    public GameObject closedPart;
     public float x;
     public float z;
 
     // Use this for initialization
     void Start () {
+        LogicMap map = new LogicMap((int)x, (int)z);
+        map.TakePosition(5, 4);
+        map.TakePosition(7, 8);
 
-        for (int i = 0; i < x; i++)
+        for (int i = 0; i < (int)x; i++)
         {
-            for (int j = 0; j < z; j++)
+            for (int j = 0; j < (int)z; j++)
             {
-                Instantiate(mapPart, new Vector3(i, 0, j), Quaternion.identity);
+                if (!map.Map[i, j])
+                {
+                    Instantiate(freePart, new Vector3(i*2, 0, j*2), Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(closedPart, new Vector3(i*2, 0, j*2), Quaternion.identity);
+                }
             }
         }
 
