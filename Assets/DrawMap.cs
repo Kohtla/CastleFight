2711e92bffddb121d0a4633f7c5cdf69eb6822ce
Kohtla <MKmoolooo@yandex.ru>
2017-07-07 12:30:03 +0400

    Класс карты и класс прорисовки карты
    Класс карты не подключен, но блоки прорисовываются.

---
+++ Assets/DrawMap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawMap : MonoBehaviour {
+    public GameObject mapPart;
+    public float x;
+    public float z;
+
+    // Use this for initialization
+    void Start () {
+
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < z; j++)
+            {
+                Instantiate(mapPart, new Vector3(i, 0, j), Quaternion.identity);
+            }
+        }
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/LogicMap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicMap{
+
+    private bool[,] data;    
+
+    public bool[,] Map
+    {
+        get
+        {
+            return data;
+        }
+
+        set
+        {
+            data = value;
+        }
+    }
+
+    public LogicMap(int x, int z)
+    {
+        Map = new bool[x, z];
+    }
+
+    public void TakePosition(int xos, int zos)
+    {
+        Map[xos, zos] = true;
+
+    }//Метод ставит true в массив
+
+    public void FreePosition(int xos, int zos)
+    {
+        Map[xos, zos] = false;
+
+    }//Метод ставит false в массив    
+}
