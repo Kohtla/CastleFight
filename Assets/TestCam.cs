24b67ac220b255c1cbbc0f33aca5f5069b5b4060
Kohtla <MKmoolooo@yandex.ru>
2017-07-08 10:47:45 +0400

    Easy камера

---
+++ Assets/TestCam.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCam : MonoBehaviour {
+
+    public GameObject camRig;
+    public float speed = 10;
+    public float X=50;
+    public float Y=50;
+    public float Z=-30;
+    Vector3 coords;
+
+	// Use this for initialization
+	void Start () 
+    {
+        camRig.transform.position = new Vector3(X, Y, Z);
+        coords = camRig.transform.position;
+                        	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            coords.z += speed*Time.deltaTime;
+            camRig.transform.position = coords;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            coords.z -= speed*Time.deltaTime;
+            camRig.transform.position = coords;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            coords.x -= speed*Time.deltaTime;
+            camRig.transform.position = coords;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            coords.x += speed*Time.deltaTime;
+            camRig.transform.position = coords;
+        }
+	
+	}
+}
